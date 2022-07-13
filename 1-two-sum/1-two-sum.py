62f86e2ef341b45ee3d5b223b4e4d7d8a2e03672
iamShafi <67453945+IamShafi@users.noreply.github.com>
2022-07-13 14:25:41 +0600

    Time: 90 ms (69.59%), Space: 15.2 MB (24.50%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,7 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #O(n^2) check every combination of two values & see if they sum up to the target
+        #O(log n) sort
+        
         values = {}
         for idx, value in enumerate(nums):
             if target - value in values: return [values[target-value], idx]
             else: values[value] = idx
-        
